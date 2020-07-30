b4db7965f396cbc02f1ffe4d0b4126d9d533d2d1
Felipe Pereira <felipepereira2697@gmail.com>
2020-07-30 21:27:11 +0100

    Creating Video page

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -2,18 +2,30 @@ import React from 'react';
 
 import Menu from '../Menu';
 import Footer from '../Footer';
+import styled from 'styled-components';
 
+const Main = styled.main`
+    background-color: var(--black);
+    color: var(--white );
+    flex:1;
+    padding-top: 50px;
+    padding-right: 5%;
+    padding-left: 5%;
+`;
 
 function PageDefault(props) {
     return (
         // Todo componente deve ter uma estrutura em volta
-        <div>
+        // <> chamamos de fragment
+        <>
 
             <Menu />
-                {props.children}
+                <Main>
+                     {props.children}
+                </Main>
             <Footer />
 
-        </div>
+        </>
 
     );
 }
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom';
 
  function CadastroVideo() {
     return (
         <PageDefault>
-            Pagina de cadastro de video
+            <h1>Cadastro de vídeo </h1>
+
+            <Link to="cadastro/categoria">
+                Cadastrar Categoria
+            </Link>
         </PageDefault>    
     );
  }
