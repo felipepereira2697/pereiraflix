5e02159479ecffcc965322d95439e86435e6f533
Felipe Pereira <felipepereira2697@gmail.com>
2020-08-01 19:41:10 +0100

    Adding PropType

--- src/components/Carousel/components/FormField/index.js
+++ src/components/Carousel/components/FormField/index.js
@@ -1,19 +1,56 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 function FormField({ value, onChange, name, type, label}) {
-    return (
-        <div>
-            <label>{label}:
-                <input 
-                type={type} 
-                value={value}
-                name = {name}
-                onChange={onChange}
-                /> 
-            </label>
-        </div>
-        )
+    const fieldId = `id_${name}`;
+    if( name === 'descricao') {
+        return (
+            
+            <div>
+                <label>{label}:
+                    <textarea 
+                    id={fieldId}
+                    type={type} 
+                    value={value}
+                    name = {name}
+                    onChange={onChange}
+                    /> 
+                </label>
+            </div>
+        );
+    }else{
+            return (
+                
+                <div>
+                    <label>{label}:
+                        <input 
+                        id={fieldId}
+                        type={type}  
+                        value={value}
+                        name = {name}
+                        onChange={onChange}
+                        /> 
+                    </label>
+                </div>
+            );
+        
+    }
 }
+
+FormField.defaultProps = {
+    type: 'text',
+    value: '',
+    onChange: () => {}, 
+}
+
+FormField.propTypes = {
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string, 
+    onChange: PropTypes.func, 
+    name:PropTypes.string.isRequired, 
+    type: PropTypes.string, 
+    
+}
+
 
 export default FormField; 
