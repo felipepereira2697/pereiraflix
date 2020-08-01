f9ef370cd22bf1ba74e39d1a2fc123b2ba54d7f0
Felipe Pereira <felipepereira2697@gmail.com>
2020-08-01 13:43:54 +0100

    useState and creating new movie categories

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -3,39 +3,78 @@ import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
  function CadastroCategoria() {
-    const [categorias, setCategorias] = useState(['Teste']);
+     const valoresIniciais = {
+         nome: 'Categoria inicial',
+         descricao: 'Essa é a descrição inicial',
+         cor: '#000'
+     }
+    const [categorias, setCategorias] = useState([]);
      //abrindo oq esta vindo do array useState
-    let [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes'); 
+    const [values, setValues] = useState(valoresIniciais); 
+
+    function setValue(chave, valor) {
+        setValues({
+            ...values,
+            [chave] : valor, //nome : 'oNomeVemAqui'
+        })
+    }
+
+    function handleChange(event) {
+        //quem é o alvo da mudança que estamos fazendo
+        const evtValue = event.target.value;
+        setValue(event.target.getAttribute('name'),evtValue);
+            
+    }
     return (
         <PageDefault>
-            <h1>Cadastro de categoria: {nomeDaCategoria} </h1>
+            <h1>Cadastro de categoria: {values.nome} </h1>
             <form onSubmit={function handleSubmit(event){
                 event.preventDefault();
                 //podemos espalhar nossas categorias com a nova categoria no mesmo elemento
-                //...categorias --> pega tuido que vc ja tem e nao joga fora, só adiciona oq vou te passar
+                //...categorias --> pega tuido que vc ja tem e nao joga fora, só adiciona oq vou te passar 
                 setCategorias([
                     ...categorias,
-                    nomeDaCategoria
-                ])
+                    values
+                ]);
+                setValues(valoresIniciais);
             }}>
-
-                <label>Nome da categoria</label>
-                <input 
-                    type="text" 
-                    value={nomeDaCategoria}
-                    onChange={(event) => {
-                        //quem é o alvo da mudança que estamos fazendo
-                        const evtValue = event.target.value;
-                        setNomeDaCategoria(evtValue);
-                    }}
-                />
+                <div>
+                    <label>Nome da categoria
+                        <input 
+                            type="text" 
+                            value={values.nome}
+                            name = "nome"
+                            onChange={handleChange}
+                        /> 
+                    </label>
+                </div>
+                <div>
+                    <label>Descrição:
+                        <textarea
+                            type="text" 
+                            name="descricao"
+                            value={values.descricao}
+                            onChange={handleChange}
+                        /> 
+                    </label>
+                </div>  
+                <div>
+                    <label>Cor:
+                        <input
+                            type="color" 
+                            value={values.cor}
+                            name="cor"
+                            onChange={handleChange}
+                        /> 
+                    </label>
+                </div>
                 <button>Cadastrar </button>
             </form>
             <ul>
-                {categorias.map((categoria) => {
+                {categorias.map((categoria, indice) => {
                     return (
-                     <li key={categoria}>
-                         {categoria}
+                     <li key={`${categoria}${indice}`}>
+                         {categoria.nome}
                      </li>
 
                     );
