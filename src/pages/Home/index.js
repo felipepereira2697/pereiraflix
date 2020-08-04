de099343f125faf25040e74178b1bf852f6a194e
Felipe Pereira <felipepereira2697@gmail.com>
2020-08-04 23:57:59 +0100

    Restructuring Home component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
+// import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
 import categoriesRepository from '../../repositories/categorias';
+import PageDefault from '../../components/PageDefault';
 
 function Home() {
 
+  const [dadosIniciais, setDadosIniciais] = useState([]);
   useEffect(() => {
     categoriesRepository.getAllWithVideos()
     .then((categoriasComVideos) => {
-      //FALTANDO IMPLEMENTAR AQUI
-      console.log(categoriasComVideos);
+      setDadosIniciais(categoriasComVideos);
     })
     .catch((err) => {
       console.log(err.message);
@@ -20,10 +21,10 @@ function Home() {
   }, []);
 
   return (
-    <div style={{background: "#141414" }}>
+    <PageDefault>
       <Menu />
-
-      <BannerMain 
+      {JSON.stringify(dadosIniciais)}
+      {/* <BannerMain 
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={"O que é Front-End? Trabalhando na área"}
@@ -47,11 +48,11 @@ function Home() {
       <Carousel 
             ignoreFirstVideo
             category={dadosIniciais.categorias[3]}
-      />
+      /> */}
 
-      <Footer />
+      
 
-    </div>
+    </PageDefault>
   );
 }
 
